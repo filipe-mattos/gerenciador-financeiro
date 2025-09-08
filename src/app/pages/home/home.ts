a53d40a314753d9a24cd125cbaebdfda117db6a4
Filipe Magioni Mattos <filipemagioni@outlook.com>
2025-09-08 18:50:26 -0300

    Adiciona componente de mensagem quando nao tiver transacoes

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,20 +1,21 @@
 import { Component, signal } from '@angular/core';
-import { Balance } from "./components/balance/balance";
-import { TransactionItem } from "./components/transaction-item/transaction-item";
+import { Balance } from './components/balance/balance';
+import { TransactionItem } from './components/transaction-item/transaction-item';
 import { Transaction } from '../../shared/transaction/interfaces/trasaction';
 import { TransactionType } from '../../shared/transaction/enums/transactionType';
+import { NoTransactions } from './components/no-transactions/no-transactions';
 
 @Component({
   selector: 'app-home',
-  imports: [Balance, TransactionItem],
+  imports: [Balance, TransactionItem, NoTransactions],
   templateUrl: './home.html',
-  styleUrl: './home.scss'
+  styleUrl: './home.scss',
 })
 export class Home {
   transactions = signal<Transaction[]>([
-    {title: 'Aluguel', value: 500, type: TransactionType.OUTCOME },
-    {title: 'Energia', value: 150, type: TransactionType.OUTCOME },
-    {title: 'Salario', value: 2800, type: TransactionType.INCOME },
-    {title: 'Internet', value: 130, type: TransactionType.OUTCOME },
-  ])
+    { title: 'Aluguel', value: 500, type: TransactionType.OUTCOME },
+    { title: 'Energia', value: 150, type: TransactionType.OUTCOME },
+    { title: 'Salario', value: 2800, type: TransactionType.INCOME },
+    { title: 'Internet', value: 130, type: TransactionType.OUTCOME },
+  ]);
 }
